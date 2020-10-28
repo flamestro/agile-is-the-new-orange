6887d832bb38f1222cec20fa64c7e6564f9cc387
flamestro <NoReplyFlamestro@gmail.com>
2020-10-28 17:19:53 +0100

    Add getBoard endpoint to get a Board

--- src/main/java/de/flamestro/AgileIsTheNewOrange/web/BoardController.java
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/web/BoardController.java
@@ -1,4 +1,17 @@
 package de.flamestro.AgileIsTheNewOrange.web;
 
+import de.flamestro.AgileIsTheNewOrange.board.Board;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class BoardController {
+
+    @GetMapping("/board")
+    public static ResponseEntity<Board> getBoard(){
+        return ResponseEntity.ok().build();
+    }
 }
