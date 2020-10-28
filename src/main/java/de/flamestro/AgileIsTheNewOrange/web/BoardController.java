471c4372e70238f8d2cb33474fa29de0a9576568
flamestro <NoReplyFlamestro@gmail.com>
2020-10-28 17:21:39 +0100

    Add addBoard endpoint to add a Board

--- src/main/java/de/flamestro/AgileIsTheNewOrange/web/BoardController.java
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/web/BoardController.java
@@ -3,6 +3,7 @@ package de.flamestro.AgileIsTheNewOrange.web;
 import de.flamestro.AgileIsTheNewOrange.board.Board;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class BoardController {
 
     @GetMapping("/board")
     public static ResponseEntity<Board> getBoard(){
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/board")
+    public static ResponseEntity<Board> addBoard(){
         return ResponseEntity.ok().build();
     }
 }
