5146eed69928b0e787474e9d6f798ffb6af6e2ec
flamestro <noreply@kilicahmet.de>
2020-10-28 16:21:12 +0100

    Init

---
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/AgileIsTheNewOrangeApplication.java
@@ -0,0 +1,13 @@
+package de.flamestro.AgileIsTheNewOrange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileIsTheNewOrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileIsTheNewOrangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/flamestro/AgileIsTheNewOrange/AgileIsTheNewOrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.flamestro.AgileIsTheNewOrange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileIsTheNewOrangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
