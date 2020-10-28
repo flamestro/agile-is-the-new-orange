5338fc64f814ff0d895e2e06aa4e754e7f33d194
flamestro <NoReplyFlamestro@gmail.com>
2020-10-28 17:58:12 +0100

    Extend user model

--- src/main/java/de/flamestro/AgileIsTheNewOrange/user/User.java
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/user/User.java
@@ -1,4 +1,18 @@
 package de.flamestro.AgileIsTheNewOrange.user;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class User {
+    private final String name;
+    private final String pictureUrl;
+    private final Role role;
+
+    public enum Role{
+        USER, // can add boards, move cards, rename boards ...
+        ARCHIVE_MASTER, // can archive boards and all rights of a USER
+        SUPERVISOR // can delete boards and all rights of a ARCHIVE_MASTER
+    }
 }
