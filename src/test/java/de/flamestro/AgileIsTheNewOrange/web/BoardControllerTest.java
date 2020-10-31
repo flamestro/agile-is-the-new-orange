18a3e94450976bdcd52440a7ba4fa02fb57b004e
flamestro <NoReplyFlamestro@gmail.com>
2020-10-31 22:31:07 +0100

    Remove unused import

--- src/test/java/de/flamestro/AgileIsTheNewOrange/web/BoardControllerTest.java
+++ src/test/java/de/flamestro/AgileIsTheNewOrange/web/BoardControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
