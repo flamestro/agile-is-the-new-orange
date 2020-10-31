188142070a05c5900d77704b99f1d53e94e425aa
flamestro <NoReplyFlamestro@gmail.com>
2020-10-31 22:30:25 +0100

    Write IT test for BoardControllerTest

--- src/test/java/de/flamestro/AgileIsTheNewOrange/web/BoardControllerTest.java
+++ src/test/java/de/flamestro/AgileIsTheNewOrange/web/BoardControllerTest.java
@@ -1,36 +1,49 @@
 package de.flamestro.AgileIsTheNewOrange.web;
 
+import de.flamestro.AgileIsTheNewOrange.util.AbstractIntegrationTest;
 import de.flamestro.AgileIsTheNewOrange.board.Board;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-class BoardControllerTest {
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class BoardControllerTest extends AbstractIntegrationTest {
 
     @Autowired
     BoardController boardController;
 
     @Test
     void whenGetBoardIsCalled_thenReturnValidResponse() {
-        Board expectedBoard = Board.builder().name("Mock Board").build();
+        // when
+        var expectedBoard = boardController.createBoard("Mock").getBody();
+        assert expectedBoard != null;
+        var expectedBoardList = List.of(expectedBoard);
 
-        ResponseEntity<List<Board>> responseEntity = boardController.getBoard("Mock");
+        // do
+        ResponseEntity<List<Board>> result = boardController.getBoard("Mock");
+        var resultBoardList = result.getBody();
 
-        assertThat(responseEntity.getBody()).usingRecursiveComparison().isEqualTo(expectedBoard);
+        // then
+        assert resultBoardList != null;
+        assertThat(resultBoardList.get(0)).usingRecursiveComparison().isEqualTo(expectedBoardList.get(0));
     }
 
     @Test
     void whenAddBoardIsCalled_thenReturnValidResponse() {
-        Board expectedBoard = Board.builder().name("Mock Board").build();
-
+        // do
         ResponseEntity<Board> responseEntity = boardController.createBoard("Mock");
 
-        assertThat(responseEntity.getBody()).usingRecursiveComparison().isEqualTo(expectedBoard);
+        // then
+        assertThat(Objects.requireNonNull(responseEntity.getBody()).getName()).isEqualTo("Mock");
+        assertThat(Objects.requireNonNull(responseEntity.getBody()).getLanes()).isEqualTo(new HashMap<>());
+        assertThat(Objects.requireNonNull(responseEntity.getBody()).getId()).isNotBlank();
     }
 }
