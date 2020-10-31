d98b94d9714aa96082085409c9fa3231c1676351
flamestro <NoReplyFlamestro@gmail.com>
2020-10-31 21:36:46 +0100

    Add id to board

--- src/main/java/de/flamestro/AgileIsTheNewOrange/board/Board.java
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/board/Board.java
@@ -3,6 +3,7 @@ package de.flamestro.AgileIsTheNewOrange.board;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
 
 import java.util.HashMap;
 
@@ -10,6 +11,9 @@ import java.util.HashMap;
 @Builder
 @ToString
 public class Board {
+    @Id
+    public String id;
+
     private final String name;
     private final HashMap<Integer, Lane> lanes;
 }
