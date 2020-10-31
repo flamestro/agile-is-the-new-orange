6b2da4c6e03ce9b8a650e37fde2d6c329586ad99
flamestro <NoReplyFlamestro@gmail.com>
2020-10-31 22:28:57 +0100

    Add Validator annotations to Board

--- src/main/java/de/flamestro/AgileIsTheNewOrange/board/Board.java
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/board/Board.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 
 @Getter
@@ -14,6 +16,8 @@ public class Board {
     @Id
     public String id;
 
+    @NotBlank
     private final String name;
+    @NotNull
     private final HashMap<Integer, Lane> lanes;
 }
