bc4d0edd9a42681fc31d4c05630a85bb82c87888
flamestro <NoReplyFlamestro@gmail.com>
2020-11-01 22:38:13 +0100

    Add test for lane removal

--- src/test/java/de/flamestro/AgileIsTheNewOrange/board/service/LaneServiceTest.java
+++ src/test/java/de/flamestro/AgileIsTheNewOrange/board/service/LaneServiceTest.java
@@ -5,9 +5,11 @@ import de.flamestro.AgileIsTheNewOrange.board.model.Lane;
 import de.flamestro.AgileIsTheNewOrange.util.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class LaneServiceTest extends AbstractIntegrationTest {
     @Autowired
     LaneService laneService;
@@ -27,5 +29,22 @@ class LaneServiceTest extends AbstractIntegrationTest {
         assert lane.getCardList() != null;
         assert !lane.getName().isBlank();
         assertThat(board.getLanes().get(0)).usingRecursiveComparison().isEqualTo(lane);
+    }
+
+    @Test
+    void whenLaneRemoved_thenBoardDoesNotContainLane() {
+        // when
+        Board board = boardService.createBoard("test_board");
+        Lane lane = laneService.createLane("test_lane", board);
+
+        // do
+        assert board.getLanes().get(0).equals(lane);
+        laneService.removeLane(lane, board);
+
+        // then
+        assert lane.getId() != null;
+        assert lane.getCardList() != null;
+        assert !lane.getName().isBlank();
+        assertThat(board.getLanes().size()).isEqualTo(0);
     }
 }
