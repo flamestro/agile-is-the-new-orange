26d71350d0967547a4024a1d61779899f37ff22a
flamestro <NoReplyFlamestro@gmail.com>
2020-11-01 19:22:31 +0100

    DRY the BoardController

--- src/main/java/de/flamestro/AgileIsTheNewOrange/web/BoardController.java
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/web/BoardController.java
@@ -10,32 +10,32 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/board")
 @AllArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
 
     // TODO: Think of a better mapping -> get by name might not be the best way for /board
-    @GetMapping("/board")
+    @GetMapping("/")
     public ResponseEntity<List<Board>> getBoardByName(@Param("name") String name){
         List<Board> board =  boardService.getBoardByName(name);
         return ResponseEntity.ok(board);
     }
 
-    @GetMapping("/board/{id}")
-    public ResponseEntity<Board> getBoardById(@PathVariable String id){
-        Board board = boardService.getBoardById(id);
-        return ResponseEntity.ok(board);
-    }
-
-    @PostMapping("/board")
+    @PostMapping("/")
     public ResponseEntity<Board> createBoard(@Param("name") String name){
         Board board = boardService.createBoard(name);
         return ResponseEntity.ok(board);
     }
 
-    @DeleteMapping("/board/{id}")
+    @GetMapping("/{id}")
+    public ResponseEntity<Board> getBoardById(@PathVariable String id){
+        Board board = boardService.getBoardById(id);
+        return ResponseEntity.ok(board);
+    }
+
+    @DeleteMapping("/{id}")
     public ResponseEntity<Board> removeBoardById(@PathVariable String id){
         boardService.removeBoard(id);
         return ResponseEntity.ok().build();
