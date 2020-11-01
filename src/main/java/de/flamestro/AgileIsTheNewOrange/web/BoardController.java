0d78d065592760e6c7197598287c8898fe778ec0
flamestro <NoReplyFlamestro@gmail.com>
2020-11-01 19:51:41 +0100

    Extend BoardController with create and remove lane endpoints

--- src/main/java/de/flamestro/AgileIsTheNewOrange/web/BoardController.java
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/web/BoardController.java
@@ -1,7 +1,9 @@
 package de.flamestro.AgileIsTheNewOrange.web;
 
 import de.flamestro.AgileIsTheNewOrange.board.model.Board;
+import de.flamestro.AgileIsTheNewOrange.board.model.Lane;
 import de.flamestro.AgileIsTheNewOrange.board.service.BoardService;
+import de.flamestro.AgileIsTheNewOrange.board.service.LaneService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
@@ -15,15 +17,16 @@ import java.util.List;
 public class BoardController {
 
     private final BoardService boardService;
+    private final LaneService laneService;
 
     // TODO: Think of a better mapping -> get by name might not be the best way for /board
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity<List<Board>> getBoardByName(@Param("name") String name){
         List<Board> board =  boardService.getBoardByName(name);
         return ResponseEntity.ok(board);
     }
 
-    @PostMapping("/")
+    @PostMapping
     public ResponseEntity<Board> createBoard(@Param("name") String name){
         Board board = boardService.createBoard(name);
         return ResponseEntity.ok(board);
@@ -38,6 +41,18 @@ public class BoardController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Board> removeBoardById(@PathVariable String id){
         boardService.removeBoard(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{boardId}/lane")
+    public ResponseEntity<Lane> addLaneToBoard(@PathVariable String boardId, @Param("name") String name){
+        Lane lane = laneService.createLane(name, boardId);
+        return ResponseEntity.ok(lane);
+    }
+
+    @DeleteMapping("/{boardId}/lane/{laneId}")
+    public ResponseEntity<Lane> removeLaneFromBoard(@PathVariable String boardId, @PathVariable String laneId){
+        laneService.removeLane(laneId, boardId);
         return ResponseEntity.ok().build();
     }
 }
