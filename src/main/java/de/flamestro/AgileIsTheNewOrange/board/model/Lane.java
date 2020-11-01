a017736cbc53546e8abd99d4170b9376713fb123
flamestro <NoReplyFlamestro@gmail.com>
2020-11-01 19:10:32 +0100

    extend lane model

--- src/main/java/de/flamestro/AgileIsTheNewOrange/board/model/Lane.java
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/board/model/Lane.java
@@ -1,4 +1,24 @@
 package de.flamestro.AgileIsTheNewOrange.board.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Builder
+@ToString
 public class Lane {
+    @Id
+    public String id;
+
+    @NotBlank
+    private final String name;
+
+    @NotNull
+    private final List<Card> cardList;
 }
