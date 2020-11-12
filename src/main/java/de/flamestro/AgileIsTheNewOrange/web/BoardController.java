6433e1382e9883301c4f21535165aa389251de4f
flamestro <NoReplyFlamestro@gmail.com>
2020-11-12 21:50:43 +0100

    Add cross origin annotation

--- src/main/java/de/flamestro/AgileIsTheNewOrange/web/BoardController.java
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/web/BoardController.java
@@ -15,7 +15,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@CrossOrigin
 @RequestMapping("/board")
 @AllArgsConstructor
 public class BoardController {
