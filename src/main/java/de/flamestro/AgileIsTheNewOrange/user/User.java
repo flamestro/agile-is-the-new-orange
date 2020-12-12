7bf9fccd6f1886562c0559e34bc1d34f8f29a596
flamestro <NoReplyFlamestro@gmail.com>
2020-12-12 14:44:08 +0100

    Adjust User model

--- src/main/java/de/flamestro/AgileIsTheNewOrange/user/User.java
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/user/User.java
@@ -3,13 +3,16 @@ package de.flamestro.AgileIsTheNewOrange.user;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Builder
 public class User {
+    @NotBlank
+    private final String userId;
     private final String name;
     private final String pictureUrl;
     private final Role role;
-
     public enum Role{
         USER, // can add boards, move cards, rename boards ...
         ARCHIVE_OWNER, // can archive boards and all rights of a USER
