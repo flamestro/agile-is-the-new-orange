b22fb396690516a79af69461325082ccf64b0f81
flamestro <NoReplyFlamestro@gmail.com>
2020-12-13 23:54:16 +0100

    Reformat BoardController

--- src/main/java/de/flamestro/AgileIsTheNewOrange/web/BoardController.java
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/web/BoardController.java
@@ -30,7 +30,8 @@ public class BoardController {
     private final CardService cardService;
 
     @PostMapping
-    public ResponseEntity<BoardResponse> createBoard(@Param("name") String name, @Param("userId") String userId) {
+    public ResponseEntity<BoardResponse> createBoard(@Param("name") String name,
+                                                     @Param("userId") String userId) {
         Board board = boardService.createBoard(name, userId);
         return ResponseEntity.ok(BoardResponse.builder().boards(List.of(board)).status(Status.SUCCESS).build());
     }
@@ -54,25 +55,31 @@ public class BoardController {
     }
 
     @PostMapping("/{boardId}/lane")
-    public ResponseEntity<LaneResponse> createLane(@PathVariable String boardId, @Param("name") String name) {
+    public ResponseEntity<LaneResponse> createLane(@PathVariable String boardId,
+                                                   @Param("name") String name) {
         Lane lane = laneService.createLaneInBoard(boardService.getBoardById(boardId), name);
         return ResponseEntity.ok(LaneResponse.builder().lane(lane).status(Status.SUCCESS).build());
     }
 
     @DeleteMapping("/{boardId}/lane/{laneId}")
-    public ResponseEntity<LaneResponse> deleteLane(@PathVariable String boardId, @PathVariable String laneId) {
+    public ResponseEntity<LaneResponse> deleteLane(@PathVariable String boardId,
+                                                   @PathVariable String laneId) {
         laneService.removeLaneFromBoard(boardService.getBoardById(boardId), laneId);
         return ResponseEntity.ok(LaneResponse.builder().status(Status.SUCCESS).build());
     }
 
     @PostMapping("/{boardId}/lane/{laneId}/card")
-    public ResponseEntity<CardResponse> createCard(@PathVariable String boardId, @PathVariable String laneId, @Param("name") String name) {
+    public ResponseEntity<CardResponse> createCard(@PathVariable String boardId,
+                                                   @PathVariable String laneId,
+                                                   @Param("name") String name) {
         Card card = cardService.createCard(boardService.getBoardById(boardId), laneId, name);
         return ResponseEntity.ok(CardResponse.builder().card(card).status(Status.SUCCESS).build());
     }
 
     @DeleteMapping("/{boardId}/lane/{laneId}/card/{cardId}")
-    public ResponseEntity<CardResponse> deleteCard(@PathVariable String boardId, @PathVariable String laneId, @PathVariable String cardId) {
+    public ResponseEntity<CardResponse> deleteCard(@PathVariable String boardId,
+                                                   @PathVariable String laneId,
+                                                   @PathVariable String cardId) {
         Card card = cardService.removeCard(boardService.getBoardById(boardId), laneId, cardId);
         return ResponseEntity.ok(CardResponse.builder().card(card).status(Status.SUCCESS).build());
     }
