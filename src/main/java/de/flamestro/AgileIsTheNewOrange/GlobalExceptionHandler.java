d40bca11a350931d95b11c8149a40fff7ff98344
flamestro <NoReplyFlamestro@gmail.com>
2020-12-13 23:54:59 +0100

    Remove unneeded Order from ControllerAdvice

--- src/main/java/de/flamestro/AgileIsTheNewOrange/GlobalExceptionHandler.java
+++ src/main/java/de/flamestro/AgileIsTheNewOrange/GlobalExceptionHandler.java
@@ -3,14 +3,11 @@ package de.flamestro.AgileIsTheNewOrange;
 import de.flamestro.AgileIsTheNewOrange.exceptions.InvalidNameException;
 import de.flamestro.AgileIsTheNewOrange.web.model.ApiError;
 import de.flamestro.AgileIsTheNewOrange.web.model.Status;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
